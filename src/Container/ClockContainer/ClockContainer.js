976cfcedb7d2f09106b16ec4a193e0d7c7f874ef
ohohoi <ohohoi@sookmyung.ac.kr>
2020-11-25 03:34:50 +0900

    feat: create timer control functions

--- src/Container/ClockContainer/ClockContainer.js
+++ src/Container/ClockContainer/ClockContainer.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './ClockContainer.css'
 import ClockRed from '../../Presentational/ClockRed/ClockRed'
+import store from '../../store/store'
+import * as timerStates from '../../store/TimerStates'
 
 const ClockCarrier = () => {
+    let currTimerState = timerStates.STOP
+
+    const startTimer = () => {
+        console.log('start');
+    }
+
+    const pauseTimer = () => {
+        console.log('pause');
+    }
+
+    const stopTimer = () => {
+        console.log('stop');
+    }
+
+    const controlTimer = () => {
+        const newTimerState = store.getState().updateTimerState.timerState
+        
+        if (newTimerState !== currTimerState) {
+            currTimerState = newTimerState
+
+            switch(newTimerState) {
+                case timerStates.START:
+                    startTimer()
+                    break
+                    
+                case timerStates.PAUSE:
+                    pauseTimer()
+                    break
+
+                case timerStates.STOP:
+                    stopTimer()
+                    break
+
+                default:
+                    break
+
+            }
+        }
+    }
+
+    const subscribe = () => {
+        store.subscribe(controlTimer)
+    }
+
+    useEffect(subscribe)
+
     return (
         <div id="clock-container">
             <ClockRed/>
