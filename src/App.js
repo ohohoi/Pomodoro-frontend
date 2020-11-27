36ab9e4ee387b2a4befc6d011948bb94a93dd999
ohohoi <ohohoi@sookmyung.ac.kr>
2020-11-27 16:56:47 +0900

    feat: add copyright footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import BtnCarrier from './container/BtnContainer/BtnContainer'
 import ClockCarrier from './container/ClockContainer/ClockContainer'
+import Footer from './presentational/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <ClockCarrier/>
       <BtnCarrier/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/Presentational/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div id="footer">
+            <div className="copyright">Copyright 2020. Gyeonglim Bang</div>
+            <div className="copyright">All rights reserved.</div>
+        </div>
+    );
+};
+
+export default Footer;
