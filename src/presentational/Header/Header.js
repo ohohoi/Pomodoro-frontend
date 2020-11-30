853a543f506e111ca6816e9e992cd6560942eb35
ohohoi <ohohoi@sookmyung.ac.kr>
2020-12-01 03:53:34 +0900

    feat: create menu drawer

--- src/presentational/Header/Header.js
+++ src/presentational/Header/Header.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react'
 import './Header.css'
 import logoSrc from '../../resources/img/pomodoro-1.png'
+import menuSrc from '../../resources/img/menu-icon.png'
+import { Button, Drawer } from '@material-ui/core'
 
 const Header = () => {
+    const menu = [
+        {
+            type: "about",
+            display: "About"
+        },
+        {
+            type: "login",
+            display: "Login"
+        }
+    ]
+    let [toggle, setToggle] = useState(false)
+
+    const toggleDrawer = () => setToggle(!toggle)
+
     return (
         <div id="header">
-            <img className="header-logo" src={ logoSrc } alt="pomodoro logo"/>
+            <img id="header-logo" src={ logoSrc } alt="pomodoro logo"/>
+            <div id="header-right">
+                <Button onClick={ toggleDrawer }>
+                    <img id="header-menu-icon" src={ menuSrc } alt="menu"/>
+                </Button>
+
+                <Drawer open={ toggle } onClose={ toggleDrawer } anchor="right">
+                    <div id="drawer-menu">
+                        {
+                            menu.map((d, i) => { return (
+                                <div id="menu" key={ i }>
+                                    { d.display }
+                                </div>
+                            )})
+                        }
+                    </div>
+                </Drawer>
+            </div>
         </div>
     );
 };
