df17c11e12fb6d6654b42b5b5c7946f8ba5bc5ef
ohohoi <ohohoi@sookmyung.ac.kr>
2020-12-06 00:58:06 +0900

    feat: create settings container

---
+++ src/container/SettingsContainer/SettingsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SettingsContainer.css'
+
+const SettingsContainer = () => {
+    return (
+        <div id="settings-container">
+        </div>
+    )
+}
+
+export default SettingsContainer
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ClockContainer from '../container/ClockContainer/ClockContainer'
 import BtnContainer from '../container/BtnContainer/BtnContainer'
+import SettingsContainer from '../container/SettingsContainer/SettingsContainer'
 
 const Home = () => {
     return (
         <div>
             <ClockContainer/>
             <BtnContainer/>
+            <SettingsContainer/>
         </div>
     )
 }
